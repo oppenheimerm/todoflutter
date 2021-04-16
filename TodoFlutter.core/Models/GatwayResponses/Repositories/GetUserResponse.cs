15ee2be814448adcb144764ac078b9a538614c47
michieloppenheimer <michieloppenheimer@gmail.com>
2021-04-16 17:37:42 +0100

    accountController /refreshToken

---
+++ TodoFlutter.core/Models/GatwayResponses/Repositories/GetUserResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoFlutter.core.Models.DTO;
+
+namespace TodoFlutter.core.Models.GatwayResponses.Repositories
+{
+    public class GetUserResponse : BaseGatewayResponse
+    {
+        public string Email { get; }
+        public string Firstname { get; }
+        public string UserId { get; }
+
+        public IEnumerable<ToDoDTO> ToDoDTOs { get; set; }
+
+        public GetUserResponse(
+            string email,
+            string firstName,
+            string userId,
+            bool success = false,
+            IEnumerable<Error> errors = null,
+            string message = null
+            ):base(success, errors, message)
+        {
+            Email = email;
+            Firstname = firstName;
+            UserId = userId;
+        }
+    }
+}
